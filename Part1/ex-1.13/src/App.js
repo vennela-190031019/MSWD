306d8b14cf66addc09971a8a73bc2eb522da7beb
vennela-190031019 <74001173+vennela-190031019@users.noreply.github.com>
2021-06-01 20:37:03 +0530

    Create App.js

---
+++ Part1/ex-1.13/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+
+const App = () => {
+  const array = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(array.length).fill(0))
+  const handleClickVote = () => {
+    const vote = [...votes]
+    vote[selected] += 1
+    setVotes(vote)
+  }
+  const handleClickSelect = () => {
+    const duplicate = Math.floor(Math.random() * array.length)
+    setSelected(duplicate)
+  }
+
+  
+
+  return (
+    <>
+      <h1>Anecdote of the day</h1>
+      <h3>{array[selected]}</h3>
+      <h3>has {votes[selected]} votes</h3>
+      <Button handleClick={handleClickVote} text="vote" />
+      <Button handleClick={handleClickSelect} text="next anecdote" />
+      
+    </>
+  )
+}
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+export default App
