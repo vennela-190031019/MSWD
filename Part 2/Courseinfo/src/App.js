474fdb83b7982cbec61fd393e2751937e575a137
vennela-190031019 <74001173+vennela-190031019@users.noreply.github.com>
2021-06-10 22:00:42 +0530

    Update App.js

--- Part 2/Courseinfo/src/App.js
+++ Part 2/Courseinfo/src/App.js
@@ -1,71 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Course from './components/course'
+import React from 'react';
+import Course from './components/Courses';
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
+    const courses = [
         {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        }, 
         {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
         }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+    ];
+    return (
+        <div>
+            <h2>Web Development Curriculum</h2>
+            {courses.map(course => <Course course={course} key={course.id} />)}
+        </div>
+    );
+};
 
-  return (
-    <div>
-      <h2> Web development curriculum</h2>
-      <ul>
-        {courses.map(course => 
-          <Course key={course.id} course={course} />
-        )}
-    
-      </ul>
-    </div>
-  )
-      
-
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
-
-export default App
+export default App;
