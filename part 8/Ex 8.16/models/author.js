03de002684b41d90ec3be69b79514781f3acebe1
vennela-190031019 <74001173+vennela-190031019@users.noreply.github.com>
2021-06-16 21:20:14 +0530

    Add files via upload

--- part 8/Ex 8.16/models/author.js
+++ part 8/Ex 8.16/models/author.js
@@ -2,23 +2,19 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 const schema = new mongoose.Schema({
-  title: {
+  name: {
     type: String,
     required: true,
     unique: true,
-    minlength: 2
+    minlength: 4
   },
-  published: {
+  born: {
     type: Number,
   },
-  author: {
-    type: String,
-    minlength: 2
+  bookCount: {
+  	type: Number,
   },
-  genres: [
-    { type: String}
-  ]
 })
 
 schema.plugin(uniqueValidator)
-module.exports = mongoose.model('Book', schema)
+module.exports = mongoose.model('Author', schema)
--- part 8/Ex 8.16/models/user.js
+++ part 8/Ex 8.16/models/user.js
@@ -2,23 +2,13 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 const schema = new mongoose.Schema({
-  title: {
+  username: {
     type: String,
     required: true,
     unique: true,
-    minlength: 2
+    minlength: 3
   },
-  published: {
-    type: Number,
-  },
-  author: {
-    type: String,
-    minlength: 2
-  },
-  genres: [
-    { type: String}
-  ]
 })
 
 schema.plugin(uniqueValidator)
-module.exports = mongoose.model('Book', schema)
+module.exports = mongoose.model('User', schema)
